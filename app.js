28e9fb72b3bba07ba7b6cd51b5b6ffebafa0cd01
Ilya Safronov <ilya.safronov@finstarlabs.ru>
2019-07-23 16:38:02 +0300

    add cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require('cors');
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const commonRouter = require("./routes/common");
 
 const app = express();
+app.use(cors());
 
 app.use(logger("dev"));
 app.use(express.json());
