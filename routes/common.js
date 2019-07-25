c8eac3b2c74ef2d206628e5a99476108cb3f82aa
Ilya Safronov <ilya.safronov@finstarlabs.ru>
2019-07-25 13:44:52 +0300

    refactor nested promises

--- routes/common.js
+++ routes/common.js
@@ -5,8 +5,8 @@ const redis = require("redis");
 const { promisify } = require("util");
 
 const client = redis.createClient();
-const getAsync = promisify(client.get).bind(client);
-const setAsync = promisify(client.set).bind(client);
+const redisGet = promisify(client.get).bind(client);
+const redisSet = promisify(client.set).bind(client);
 
 const API_ROUTE = process.env.API_ROUTE || "http://10.5.0.178:9999/";
 
@@ -14,52 +14,43 @@ const ax = axios.create({
   baseURL: API_ROUTE
 });
 
-async function fromApi(route, res) {
-  return ax
-    .get(route)
-    .then(response => response.data)
-    .catch(error => {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      if (error.response) {
-        console.log(
-          `StatusCode: ${error.response.status} : ${JSON.stringify(
-            error.response.data
-          )}`
-        );
-        res.error(error.response.data);
-        return error.response.data;
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        // http.ClientRequest in node.js
-      } else if (error.request) {
-        console.log(error.request);
-        res.error(error.request);
-      }
-      Promise.reject();
-    });
-}
-
-async function getRedis(route, res) {
-  return getAsync(route).then(async result => {
-    if (!result) {
-      return await fromApi(route, res).then(apiData => {
-          const responseString = JSON.stringify(apiData);
-        console.log("api data");
-        setAsync(route, responseString);
-        return responseString;
-      });
-    }
-    return result;
-  });
+async function processApiResponse(url, apiResponse) {
+  const responseString = JSON.stringify(apiResponse.data);
+  await redisSet(url, responseString);
+  return responseString;
 }
 
 /* GET home page. */
-router.get("*", function(req, res) {
-  (async function() {
-    const data = await getRedis(req.url, res);
-      res.send(data);
-  })();
-});
+router.get("*", async function(req, res, next) {
+  const redisResponse = await redisGet(req.url);
+  if (redisResponse) {
+    res.send(redisResponse);
+  } else {
+    const apiResponse = ax
+        .get(req.url)
+        .then(apiResponse => {
+          return processApiResponse(req.url, apiResponse);
+        })
+        .then(processedApiResponse => {
+          res.send(processedApiResponse);
+        })
+        .catch(error => {
+          if (error.response) {
+            console.log(
+                `StatusCode: ${error.response.status} : ${JSON.stringify(
+                    error.response.data
+                )}`
+            );
 
+            res.send(error.response.data);
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+          } else if (error.request) {
+            console.log(error.request);
+            next();
+          }
+        });
+  }
+});
 module.exports = router;
